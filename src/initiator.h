cf9b55db6ba40a9c786fb802e58c9cfb1ca7178c
Vishesh Handa <me@vhanda.in>
2014-08-11 16:06:02 +0200

    Add a sample initiator class with a mock
    Just for testing

---
+++ src/initiator.h
@@ -0,0 +1,39 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2014  Vishesh Handa <me@vhanda.in>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef INITIATOR_H
+#define INITIATOR_H
+
+class AbstractInitator {
+public:
+    virtual ~AbstractInitator() {}
+    virtual bool test() = 0;
+};
+
+/**
+ * This class decides which the starting class should be
+ */
+class Initiator : public AbstractInitator
+{
+public:
+    bool test() { return true; }
+};
+
+#endif // INITIATOR_H
