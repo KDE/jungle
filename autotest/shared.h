38563802ed015d5a8f6bb8d782c321fa7c0c31e6
Àlex Fiestas <afiestas@kde.org>
2014-08-13 12:07:25 +0200

    Move QTEST_GMOCK_MAIN into its own file

--- autotest/shared.h
+++ autotest/shared.h
@@ -1,5 +1,4 @@
 /*
- * <one line to give the library's name and an idea of what it does.>
  * Copyright (C) 2014  Vishesh Handa <me@vhanda.in>
  *
  * This library is free software; you can redistribute it and/or
@@ -18,43 +17,13 @@
  *
  */
 
-#include "shared.h"
-
-#include "../src/interfaces/IInitiator.h"
-#include "../src/jungleapp.h"
-
-#include "gmock/gmock.h"
-
-#include <QTest>
-#include <QDebug>
-
-#include <KConfig>
-
-class MockInitiator : public AbstractInitiator
-{
-public:
-    MOCK_METHOD1(init, void(KConfig *));
-};
-
-class JungleAppTest : public QObject
-{
-    Q_OBJECT
-
-private Q_SLOTS:
-    void initTest();
-};
-
-void JungleAppTest::initTest()
-{
-    KConfig *config = new KConfig();
-    MockInitiator initiator;
-    EXPECT_CALL(initiator, init(config)).Times(1);
-
-    JungleApp app(config, &initiator);
-
-    app.init();
+#define QTEST_GMOCK_MAIN(TestObject) \
+int main(int argc, char *argv[]) \
+{ \
+    QCoreApplication app(argc, argv); \
+    app.setAttribute(Qt::AA_Use96Dpi, true); \
+    ::testing::GTEST_FLAG(throw_on_failure) = true; \
+    ::testing::InitGoogleMock(&argc, argv); \
+    TestObject tc; \
+    return QTest::qExec(&tc, argc, argv); \
 }
-
-QTEST_GMOCK_MAIN(JungleAppTest)
-
-#include "jungleapptest.moc"
