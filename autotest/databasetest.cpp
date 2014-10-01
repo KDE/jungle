8df74aeb3d1efb114e1cb5de5924f9545a2a0947
Vishesh Handa <me@vhanda.in>
2014-10-01 02:32:40 +0200

    Add a database which uses EJDB internally
    We could really use a nice wrapper over this database, like we have
    qsql. For now this just stores QVariantMaps and retrieves them with the
    id specified.

---
+++ autotest/databasetest.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2014  Vishesh Handa <vhanda@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "database.h"
+#include "shared.h"
+
+#include <QTest>
+#include <QVariantMap>
+#include <QDebug>
+#include <QTemporaryDir>
+
+class DatabaseTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void test();
+};
+
+using namespace Jungle;
+
+void DatabaseTest::test()
+{
+    QVariantMap data;
+    data["type"] = "episode";
+    data["mimetype"] = "video/mp4";
+    data["series"] = "Outlander";
+    data["episodeNumber"] = 5;
+
+    QTemporaryDir dir;
+    Database db(dir.path() + "/db");
+
+    QByteArray id = db.add(data);
+    QVariantMap output = db.fetch(id);
+
+    data["_id"] = QString::fromUtf8(id);
+    QCOMPARE(output, data);
+}
+
+QTEST_GMOCK_MAIN(DatabaseTest);
+
+#include "databasetest.moc"
