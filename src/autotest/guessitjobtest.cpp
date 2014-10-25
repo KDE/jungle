fea180f872ded39a52a1cca5ffe2aa23e575052d
Vishesh Handa <me@vhanda.in>
2014-10-25 21:45:34 +0200

    Add a GuessItJob test

---
+++ src/autotest/guessitjobtest.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2014  Vishesh Handa <me@vhanda.in>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "../guessitjob.h"
+
+#include <QTest>
+#include <QVariantMap>
+#include <QSignalSpy>
+#include <QDebug>
+
+class GuessItJobTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testEpisode();
+};
+
+using namespace Jungle;
+
+void GuessItJobTest::testEpisode()
+{
+    const QString url("/home/vishesh/Outlander.S01E05.HDTV.x264-KILLERS.mp4");
+
+    QVariantMap data;
+    data["type"] = "episode";
+    data["releaseGroup"] = "KILLERS";
+    data["mimetype"] = "video/mp4";
+    data["series"] = "Outlander";
+    data["episodeNumber"] = 5;
+    data["videoCodec"] = "h264";
+    data["format"] = "HDTV";
+    data["season"] = 1;
+    data["container"] = "mp4";
+
+    GuessItJob* job = new GuessItJob(url);
+    QSignalSpy spy(job, SIGNAL(finished(GuessItJob*)));
+    spy.wait();
+
+    QCOMPARE(job->data(), data);
+}
+
+QTEST_MAIN(GuessItJobTest);
+
+#include "guessitjobtest.moc"
